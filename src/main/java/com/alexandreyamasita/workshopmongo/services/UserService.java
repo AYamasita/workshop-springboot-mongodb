4727bfa7bcfa2a741a9b2bd8d517f3b22240951a
ayamasita <alexandreyamasita@gmail.com>
2019-04-29 10:56:20 -0300

    Inserção de usuário com POST

--- src/main/java/com/alexandreyamasita/workshopmongo/services/UserService.java
+++ src/main/java/com/alexandreyamasita/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alexandreyamasita.workshopmongo.domain.User;
+import com.alexandreyamasita.workshopmongo.dto.UserDTO;
 import com.alexandreyamasita.workshopmongo.respository.UserRepository;
 import com.alexandreyamasita.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,6 +26,17 @@ public class UserService  {
 	{
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Object was not found."));
+	}
+	
+	public User insert(User obj)
+	{
+		return repo.insert(obj);
+		
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+		
 	}
 
 }
