040b829b64cdb626e478c4430b205a774a744fd7
ayamasita <alexandreyamasita@gmail.com>
2019-06-08 12:36:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/alexandreyamasita/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.alexandreyamasita.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandreyamasita.workshopmongo.domain.Post;
+import com.alexandreyamasita.workshopmongo.respository.PostRepository;
+import com.alexandreyamasita.workshopmongo.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class PostService {
+	
+	@Autowired   //injeção de dependencia automatica do SpringBoot
+	private PostRepository repo;
+	
+	public Post findById(String id)
+	{
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Object was not found."));
+	}
+
+}
