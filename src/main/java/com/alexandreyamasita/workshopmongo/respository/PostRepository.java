465e01fecba2e9743ccdbfe270b85b62356379b9
ayamasita <alexandreyamasita@gmail.com>
2019-07-30 14:34:37 -0300

    @Query Consulta Simples

--- src/main/java/com/alexandreyamasita/workshopmongo/respository/PostRepository.java
+++ src/main/java/com/alexandreyamasita/workshopmongo/respository/PostRepository.java
@@ -3,11 +3,19 @@ package com.alexandreyamasita.workshopmongo.respository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.alexandreyamasita.workshopmongo.domain.Post;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options:'i'} }")   //?0 -> primeiro parâmetro -> text
+	List<Post> searchTitle(String text); //@Query 
+	//https://docs.mongodb.com/manual/reference/operator/query/regex/
+	
 	List<Post>findByTitleContainingIgnoreCase(String text); //QueryMethod
+	//https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/ 
 	
 }
